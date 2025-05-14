8891c97c5af7703e57e5ef55ce3425f6a08ea5b4
Kiobus <kiobus@kiobus.com>
2025-05-14 17:00:07 +0200

    [NEW] Excepciones creadas en sus carpetas correspondientes

---
+++ lib/exceptions/authentication_exceptions.dart
@@ -0,0 +1,13 @@
+class TokenExpiredException implements Exception {
+  final String message;
+  TokenExpiredException(this.message);
+  @override
+  String toString() => message;
+}
+
+class AuthenticationException implements Exception {
+  final String message;
+  AuthenticationException(this.message);
+  @override
+  String toString() => message;
+}
